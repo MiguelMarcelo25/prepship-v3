1c3334ffd44c2f9325d8070e46c864b8612ec0e0
MiguelMarcelo25 <miguelmarcelooo371225@gmail.com>
2026-04-28 16:24:03 +0800

    Show marked-up base cost in red

--- web/src/components/Views/OrdersView.tsx
+++ web/src/components/Views/OrdersView.tsx
@@ -804,7 +804,7 @@ function renderRateAmountWithMarkup(baseAmount: number | null, markedAmount: num
     <div style={{ lineHeight: 1.15 }}>
       <strong style={{ color: 'var(--green)', fontSize: 12 }}>{formatMoney(displayAmount)}</strong>
       {hasMarkup ? (
-        <div style={{ fontSize: 10, color: '#111827', fontWeight: 600 }}>{formatMoney(baseAmount)}</div>
+        <div style={{ fontSize: 10, color: 'var(--red)', fontWeight: 600 }}>{formatMoney(baseAmount)}</div>
       ) : null}
     </div>
   )
